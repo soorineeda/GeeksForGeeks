09ace62a48074312567c51e0159a8018f1fe6e3c
Narender Soorineeda <narender.soorineeda@gmail.com>
2017-11-11 16:38:05 -0600

    Maximum product sub array

---
+++ src/arrays/MaximumProductSubArray.java
@@ -0,0 +1,67 @@
+package arrays;
+
+public class MaximumProductSubArray {
+    public static int findMaxProduct(int[] arr){
+        if(arr.length == 0){
+            return 0;
+        }
+
+        int result = arr[0];
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i=1; i< arr.length; i++){
+            //handling a case where arr[i] = 0
+            if(arr[i] == 0) {
+                max = 0;
+                min = 0;
+            }
+
+            int temp = max;
+            max = max(arr[i], max * arr[i], min * arr[i]);
+            min = min(arr[i], temp * arr[i], min * arr[i]);
+
+            //update result
+            if(max > result){
+                result = max;
+            }
+
+//            System.out.println(max + ":" + min);
+        }
+        return result;
+    }
+
+    private static int max(int n1, int n2, int n3) {
+        if(n1 > n2){
+            return (n1 > n3)? n1 : n3;
+        }
+
+        return (n2 > n3)? n2 : n3;
+    }
+
+    private static int min(int n1, int n2, int n3) {
+        if(n1 < n2){
+            return (n1 < n3)? n1 : n3;
+        }
+
+        return (n2 < n3)? n2 : n3;
+    }
+
+    public static void main(String args[]){
+/*
+        int arr[] = {6, -3, -10, 0, 2};
+        System.out.println(findMaxProduct(arr));
+
+        int arr2[] = {-1, -3, -10, 0, 60};
+        System.out.println(findMaxProduct(arr2));
+
+
+        int arr3[] = {-2, -3, 0, -2, -40};
+        System.out.println(findMaxProduct(arr3));
+
+*/
+
+        int arr4[] = {2,0,3,-2};
+        System.out.println(findMaxProduct(arr4));
+    }
+}
